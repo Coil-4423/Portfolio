a10733f1c5c4394b99a342ebb347a81897d798e5
Takehito Sumimura <tsumimura@my.bcit.ca>
2024-10-20 16:59:50 -0700

    changed configs to output export

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,   // Enable React strict mode for better debugging
-    swcMinify: true,         // Enable SWC compiler for faster builds
-    images: {
-      domains: ['sumitake.ca'],  // Allow images from specific domains
-    },
-    output: 'standalone',    // This option makes it easier to deploy on platforms like Vercel
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,   // Enable React strict mode for better debugging
+  swcMinify: true,         // Enable SWC compiler for faster builds
+  images: {
+    domains: ['sumitake.ca'],  // Allow images from specific domains
+  },
+  output: 'export',        // Enable static HTML export
+  trailingSlash: true,     // Ensure URLs work correctly with static export
+};
+
+export default nextConfig;
